2c68063e26ac60f55d0c6dd368cbe3b87823f583
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-07-22 20:21:38 +0300

    task4 is ready

--- task4/script.js
+++ task4/script.js
@@ -2,3 +2,27 @@
 
 // Если оба числа не попадают в диапазон от 100 до 300 или введено не число — выводить ниже текст «одно из чисел вне диапазона от 100 до 300»;
 // Если числа попадают в диапазон от 100 до 300 — сделать запрос c помощью fetch по URL https://picsum.photos/200/300, где первое число — ширина картинки, второе — высота.
+
+
+const btn = document.querySelector('button');
+const div = document.querySelector('.div');
+const value = document.querySelector('input').value;
+
+btn.addEventListener('click', () => {
+    const width = document.querySelector('#width').value;
+    const height = document.querySelector('#height').value;
+    console.log(width, height)
+    if (width < 100 || height < 100 || width > 300 || height > 300) {
+        div.innerHTML = '«одно из чисел вне диапазона от 100 до 300»';
+    } else {
+        sendRequest(width, height);
+    }    
+})
+
+function sendRequest(paramWidth, paramHeight) {
+    fetch(`https://picsum.photos/${paramWidth}/${paramHeight}`)
+    .then(function() {
+        div.innerHTML = `<img src="https://picsum.photos/${paramWidth}/${paramHeight}" alt="">`
+    })
+    .catch(() => console.log('error'));
+}
