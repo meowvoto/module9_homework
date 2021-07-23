cf783631008d0563efe8140cd7c4e6fbe26187e8
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-07-24 01:27:38 +0300

    request isnt working

---  task5/script.js
+++  task5/script.js
@@ -4,10 +4,25 @@ const div = document.querySelector('.div');
 btn.addEventListener('click', () => {
     const page = document.querySelector('#page').value;
     const limit = document.querySelector('#limit').value;
-    console.log(width, height)
-    if (width < 100 || height < 100 || width > 300 || height > 300) {
-        div.innerHTML = '«одно из чисел вне диапазона от 100 до 300»';
+    const checkInp1 = page < 1 || page > 10;
+    const checkInp2 = limit < 1 || limit > 10;
+    if (checkInp2 && checkInp1) {
+        div.innerHTML = 'Номер страницы и лимит вне диапазона от 1 до 10';
+    } else if (checkInp2) {
+        div.innerHTML = 'Лимит вне диапазона от 1 до 10';
+    } else if (checkInp1) {
+        div.innerHTML = 'Номер страницы вне диапазона от 1 до 10';
     } else {
-        sendRequest(width, height);
+        sendRequest(page, limit);
     }    
 })
+
+function sendRequest(p1, p2) {
+    fetch(`https://picsum.photos/v2/list?page=${p1}&limit=${p2}`)
+    .then((response) => {return response.json()})
+    .then(function(data) {
+     data.forEach(item =>
+        div.innerHTML = `<img src="${item.url}" alt="">`
+    )})
+    .catch(() => console.log('Произошла ошибка'))
+    }
