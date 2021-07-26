05a757bfaf555bb5d34b126b13b45e060999f2c5
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-07-26 16:57:57 +0300

    task 5 is ready

---  task5/script.js
+++  task5/script.js
@@ -1,28 +1,61 @@
-const btn = document.querySelector('button');
-const div = document.querySelector('.div');
+function script() {
+    const btn = document.querySelector('button');
+    const div = document.querySelector('.div');
+    const storageData = localStorage.getItem('images');
 
-btn.addEventListener('click', () => {
-    const page = document.querySelector('#page').value;
-    const limit = document.querySelector('#limit').value;
-    const checkInp1 = page < 1 || page > 10;
-    const checkInp2 = limit < 1 || limit > 10;
-    if (checkInp2 && checkInp1) {
-        div.innerHTML = 'Номер страницы и лимит вне диапазона от 1 до 10';
-    } else if (checkInp2) {
-        div.innerHTML = 'Лимит вне диапазона от 1 до 10';
-    } else if (checkInp1) {
-        div.innerHTML = 'Номер страницы вне диапазона от 1 до 10';
-    } else {
-        sendRequest(page, limit);
-    }    
-})
+    btn.addEventListener('click', processInput)
 
-function sendRequest(p1, p2) {
-    fetch(`https://picsum.photos/v2/list?page=${p1}&limit=${p2}`)
-    .then((response) => {return response.json()})
-    .then(function(data) {
-     data.forEach(item =>
-        div.innerHTML = `<img src="${item.url}" alt="">`;
-    )})
-    .catch(() => console.log('Произошла ошибка'))
+    window.addEventListener('load', checkStorage)
+
+    function processInput() {
+        const page = document.querySelector('#page').value;
+        const limit = document.querySelector('#limit').value;
+        const checkInp1 = page < 1 || page > 10;
+        const checkInp2 = limit < 1 || limit > 10;
+        while(div.firstChild){
+            div.removeChild(div.firstChild);
+        }
+        if (checkInp2 && checkInp1) {
+            div.innerHTML = 'Номер страницы и лимит вне диапазона от 1 до 10';
+            localStorage.clear();
+        } else if (checkInp2) {
+            div.innerHTML = 'Лимит вне диапазона от 1 до 10';
+            localStorage.clear();
+        } else if (checkInp1) {
+            div.innerHTML = 'Номер страницы вне диапазона от 1 до 10';
+            localStorage.clear();
+        } else {
+            sendRequest(page, limit);
+        }   
+    } 
+
+    function sendRequest(p1, p2) {
+        fetch(`https://picsum.photos/v2/list?page=${p1}&limit=${p2}`)
+        .then((response) => {return response.json()})
+        .then((data) => {
+            data.forEach(image => {
+                const tempImage = document.createElement('img');
+                tempImage.setAttribute('src', image.download_url);
+                div.appendChild(tempImage);
+            })
+            return data
+        })
+        .then(data => {
+            localStorage.setItem('images', JSON.stringify(data));
+        })
+        .catch(() =>  div.innerHTML = 'Произошла ошибка')
+    };
+
+    function checkStorage() {
+        if(storageData) {
+            const data = JSON.parse(storageData);
+            data.forEach(image => {
+                const tempImage = document.createElement('img');
+                tempImage.setAttribute('src', image.download_url);
+                div.appendChild(tempImage);
+            })
+        } 
     }
+}
+
+document.addEventListener('DOMContentLoaded', script)
